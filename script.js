921632d653cdbf5ab54b3b0994bbefe00914d18c
Chris <cfruci@gmail.com>
2022-09-14 17:40:24 -0600

    Rock, Paper, Scissors 2 with switch statement, prompt, and 5-round game

---
+++ script.js
@@ -0,0 +1,66 @@
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3) + 1;
+
+  switch(choice) {
+
+    case 1:
+      return "rock";
+      break;
+    case 2:
+      return "paper";
+      break;
+    case 3:
+      return "scissors";
+      break;
+    default:
+      break;
+  }
+}
+
+function getPlayerChoice() {
+  const playerChoice = prompt("Rock, Paper, or Scissors? ");
+  const playerChoiceClean = playerChoice.toLowerCase();
+  return playerChoiceClean;
+}
+
+function playRound(player, comp) {
+  console.log(`Player choice: ${player}`);
+  console.log(`Comp choice: ${comp}`);  
+  if (player === comp) {
+    return "Tie!";
+  } else if (player === "rock" && comp === "scissors" || player === "paper" && comp === "rock" || player === "scissors" && comp === "paper") {
+    return "Player wins!";
+  } else {
+    return "Computer wins!"
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let compScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const compChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+    const result = playRound(playerChoice, compChoice);
+    console.log(result);
+
+    if (result === 'Player wins!') {
+      playerScore++;
+    } else if (result === "Computer wins!") {
+      compScore++;
+    }
+    console.log(`Player score: ${playerScore}, Computer Score: ${compScore}`);
+  }
+
+  if (playerScore > compScore) {
+    return "Player wins!";
+  } else if (compScore > playerScore) {
+    return "Comp wins!"
+  } else {
+    return "It's a Tie!";
+  }
+}
+
+const finalResult = game();
+console.log(finalResult);
