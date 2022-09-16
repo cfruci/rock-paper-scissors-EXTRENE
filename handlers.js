4f4fc7d386345ade27a5e6e9199ae8c585045071
Chris <cfruci@gmail.com>
2022-09-16 10:54:08 -0600

    Fixes a lot of the visual flow in preparation for a match

--- handlers.js
+++ handlers.js
@@ -1,29 +1,37 @@
 const newMatchBtn = document.querySelector(".new-match-btn");
-// const abortBtn = document.querySelector("#abort-btn");
 const matchSetup = document.querySelector(".match-setup");
-const beginBtn = document.querySelector(".begin-game-btn");
-const weaponsDiv = document.querySelector(".weapon-selection");
-// const playerIcon = document.querySelector("#player-icon");
-// const computerIcon = document.querySelector("#computer-icon");
+const matchOptions = document.querySelector(".match-options");
+const gameAssets = document.querySelector(".game-assets");
+const abortBtn = document.querySelector(".abort-btn");
 
+// functions
 function initiateSetup() {
-  matchSetup.classList.toggle("active");
-  newMatchBtn.style.display = "None";
+  matchSetup.classList.add("active");
+  newMatchBtn.style.visibility = "Hidden";
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  prepStage();
+  const numRounds = event.target[0].value;
+  const playerAvatar = event.target[1].value;
+  const computerAvatar = event.target[2].value;
 }
 
 function prepStage() {
-  matchSetup.classList.toggle("active");
-  weaponsDiv.classList.toggle("active");
-
-  // movePlayers();
-  // displayFists();
-  // showScoreBoard();
+  matchSetup.classList.remove("active");
+  gameAssets.classList.add("active");
+  abortBtn.style.visibility = "Visible";
 }
 
-// function resetGame() {
+function resetGame() {
+  newMatchBtn.style.visibility = null;
+  gameAssets.classList.remove("active");
+  abortBtn.style.display = "Hidden";
+  // add function to reset score
+}
 
-// }
-
+// event listeners
 newMatchBtn.addEventListener("click", initiateSetup);
-beginBtn.addEventListener("click", prepStage);
-// abortBtn.addEventListener("click", resetGame);
+matchOptions.addEventListener("submit", handleSubmit);
+abortBtn.addEventListener("click", resetGame);
